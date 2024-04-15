676cac9d5f9ea78510611c6213d9c209c89ee404
uliseshm <ulises.h.dev@gmail.com>
2024-04-15 10:48:55 -0600

    prueba de ui clientes num2

--- client/src/pages/Vendedoras/Vendedora.jsx
+++ client/src/pages/Vendedoras/Vendedora.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Vendedora.css'
 import bicicleta from '../../components/bici.svg'
+import clipboard from '../../components/clipboard.svg'
 import BottomMenu from '../../components/BottomMenu'
 
 const Vendedora = () => {
@@ -16,6 +17,7 @@ const Vendedora = () => {
         <p className='puntos-vendedora'>Puntos: 21000</p>
         <p className='telefono-vendedora'>Teléfono: 00 46 46 99 01</p>
         <img src={bicicleta} alt="bicicleta" className='bici' />
+        <img src={clipboard} alt="clipboard" className='clipboard'/>
       </div>
       {/* ejemplos de aqui para abajo */}
       <div className="card-container-vendedora">
@@ -33,28 +35,28 @@ const Vendedora = () => {
         <p className='puntos-vendedora'>Puntos: 21000</p>
         <p className='telefono-vendedora'>Teléfono: 00 46 46 99 01</p>
       </div> */}
-      {/* <div className="card-container-vendedora">
+      <div className="card-container-vendedora">
         <h3 className='nombre-vendedora'>Nombre Vendedora</h3>
         <p className='direccion-vendedora'>Dirección: Monte Olimpo #329</p>
-        <p className='ruta-vendedora'>Ruta: DGO</p>
+        <p className='ruta-vendedora'>Ruta: COL</p>
         <p className='puntos-vendedora'>Puntos: 21000</p>
         <p className='telefono-vendedora'>Teléfono: 00 46 46 99 01</p>
-      </div> */}
-
-      {/* <div className="card-container-vendedora">
-        <h3 className='nombre-vendedora'>Nombre Vendedora</h3>
-        <p className='direccion-vendedora'>Dirección: Monte Olimpo #329</p>
-        <p className='ruta-vendedora'>Ruta: DGO</p>
-        <p className='puntos-vendedora'>Puntos: 21000</p>
-        <p className='telefono-vendedora'>Teléfono: 00 46 46 99 01</p>
-      </div> */}
+      </div>
 
       <div className="card-container-vendedora">
         <h3 className='nombre-vendedora'>Nombre Vendedora</h3>
         <p className='direccion-vendedora'>Dirección: Monte Olimpo #329</p>
-        <p className='ruta-vendedora'>Ruta: DGO</p>
+        <p className='ruta-vendedora'>Ruta: SLP</p>
         <p className='puntos-vendedora'>Puntos: 21000</p>
         <p className='telefono-vendedora'>Teléfono: 00 46 46 99 01</p>
+      </div>
+
+      <div className="card-container-vendedora">
+        <h3 className='nombre-vendedora'>Nombre Vendedora</h3>
+        <p className='direccion-vendedora'><span>Dirección:</span> Monte Olimpo #329</p>
+        <p className='ruta-vendedora'><span>Ruta:</span> DGO</p>
+        <p className='puntos-vendedora'><span>Puntos:</span> 21000</p>
+        <p className='telefono-vendedora'><span>Teléfono:</span> 00 46 46 99 01</p>
       </div>
       </section>
 
